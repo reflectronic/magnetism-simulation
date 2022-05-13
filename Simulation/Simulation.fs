125aea0d479cc6e36258d751007256439b71ee95
reflectronic <john-tur@outlook.com>
2022-05-13 09:36:05 -0400

    Add project files.

---
+++ Simulation/Simulation.fs
@@ -0,0 +1,64 @@
+﻿namespace Simulation
+
+open System.Numerics
+open type System.Numerics.Vector3
+open type System.MathF
+
+module Calculate =
+    let Mu = (4f * PI * 10e-7f)
+
+    let B M r = (Mu / (4f * PI)) * (3f * (Dot(M, r) * r) / (r.Length() ** 5f) - (M / (r.Length() ** 3f)))
+
+    let U m B = Dot(-m, B)
+
+    let potentialField magneticMomentSmall magneticMomentBig sideLength =
+        let stepping = 1f // cm
+        let toPosition index = (float32 index) * stepping
+        Array3D.init sideLength sideLength sideLength (fun x y z -> 
+            U magneticMomentSmall 
+            <| B magneticMomentBig (Vector3(toPosition x, toPosition y, toPosition z)))
+
+    [<Struct>]
+    type Dimension = X | Y | Z
+
+    let forceVectorField potentials =
+        let edgeIndex dimension index =
+            index = 0 || index + 1 = match dimension with 
+                                     | X -> Array3D.length1 potentials
+                                     | Y -> Array3D.length2 potentials
+                                     | Z -> Array3D.length3 potentials
+
+        let product upperBound exclusions fn = 
+            seq { 0..upperBound } 
+            |> Seq.except exclusions 
+            |> Seq.map (fun i -> fn i) 
+            |> Seq.fold (fun partialProduct factor -> partialProduct * factor) 1
+
+        let sum upperBound exclusions fn =
+            seq { 0..upperBound }
+            |> Seq.except exclusions
+            |> Seq.sumBy (fun i -> fn i)
+
+        let l' j n = 
+            let numerator = (sum n (Seq.singleton j) 
+                (fun k -> 
+                    product n [| k; j |] (fun l -> (n / 2) - l))) 
+            let denominator = product n (Seq.singleton j) (fun k -> j - k);
+            float32 numerator / float32 denominator
+        
+        let L' yVals = 
+            yVals 
+            |> Array.mapi (fun i y -> y * float32 (l' i (yVals.Length - 1))) 
+            |> Array.sum
+                    
+        let approximateGradient x y z dimension =
+            match dimension with
+            | X -> L' [| potentials.[x - 1, y, z]; potentials.[x, y, z]; potentials.[x + 1, y, z] |]
+            | Y -> L' [| potentials.[x, y - 1, z]; potentials.[x, y, z]; potentials.[x, y + 1, z] |]
+            | Z -> L' [| potentials.[x, y, z - 1]; potentials.[x, y, z]; potentials.[x, y, z + 1] |]
+
+        potentials |> Array3D.mapi (fun x y z _ -> 
+            if edgeIndex X x || edgeIndex Y y || edgeIndex Z z then 
+                Vector3() 
+            else 
+                -Vector3(approximateGradient x y z X, approximateGradient x y z Y, approximateGradient x y z Z))
---
+++ SimulationUI/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using HelixToolkit.Wpf;
+
+using Simulation;
+
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace SimulationUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var vectorField = Calculate.forceVectorField(
+                Calculate.potentialField(
+                    magneticMomentSmall: new(1, 1, 1),
+                    magneticMomentBig: new(0, 0, 30_000),
+                    sideLength: 16));
+
+            var maxMagnitude = vectorField.OfType<Vector3>().MaxBy(v => v.Length()).Length();
+            var minMagnitude = vectorField.OfType<Vector3>().MinBy(v => v.Length()).Length();
+
+
+            for (int x = 0; x < vectorField.GetLength(0); x++)
+            {
+                for (int y = 0; y < vectorField.GetLength(1); y++)
+                {
+                    for (int z = 0; z < vectorField.GetLength(2); z++)
+                    {
+                        var direction = vectorField[x, y, z];
+                        var originVector = new Vector3D(x, y, z) * 1.5;
+                        var directionVector = new Vector3D(direction.X, direction.Y, direction.Z);
+                        directionVector.Normalize();
+
+                        var longerThanMin = direction.Length() - minMagnitude;
+                        var rChannelColor = (byte)Math.Min(longerThanMin / (maxMagnitude - minMagnitude) * 255, 255);
+
+                        var fill = new SolidColorBrush(Color.FromArgb(255, rChannelColor, 0, 0));
+                        fill.Freeze();
+
+                        var mat = new DiffuseMaterial(fill);
+                        mat.Freeze();
+
+                        var arrow = new ArrowVisual3D
+                        {
+                            Point1 = (Point3D)originVector,
+                            Point2 = (Point3D)(originVector + directionVector),
+                            Diameter = 0.1,
+                            Material = mat
+                        };
+
+                        Viewport.Children.Add(arrow);
+                    }
+                }
+            }
+        }
+    }
+}
